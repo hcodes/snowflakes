61b41c09968c56c42781d7dedf08e4ec45489c6b
Denis Seleznev <hcodes@yandex.ru>
2017-11-12 13:39:27 +0300

    Update example

--- example/example.js
+++ example/example.js
@@ -2,12 +2,22 @@ var App = {
     params: {
         size: 'fullscreen',
         bg: 'white',
+        count: 50,
+        speed: 1,
         useRotate: true,
         useScale: true
     },
     setBg: function(value) {
         this.params.bg = value;
         this.updateSettings();
+    },
+    setCount: function(value) {
+        this.params.count = parseInt(value, 10);
+        this.redraw();
+    },
+    setSpeed: function(value) {
+        this.params.speed = parseFloat(value);
+        this.redraw();
     },
     setSize: function(value) {
         this.params.size = value;
@@ -32,6 +42,8 @@ var App = {
 
         this._snow = new Snowflakes({
             container: this.params.size === 'fullscreen' ? document.body : document.getElementById('layer'),
+            count: this.params.count,
+            speed: this.params.speed,
             useRotate: this.params.useRotate,
             useScale: this.params.useScale
         });
