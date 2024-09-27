a68e4e2ad3b1186c2dd77082ce0da9d1f1248bc6
Denis Seleznev <hcodes@users.noreply.github.com>
2024-09-27 09:45:46 +0300

    Update dev deps

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    ignores: [
+      '.*',
+      'dist/**',
+      'node_modules/**',
+      'tools/**',
+      '*.config.js'
+    ]
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}']
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
